e46ecd69ede608669bb9c77409049b4a8ed8f4c3
veeramanidhanasekar <veeramanid007@gmail.com>
2022-02-05 22:26:09 +0530

    task done

---
+++ src/org/git/GitClass.java
@@ -0,0 +1,16 @@
+package org.git;
+
+public class GitClass {
+
+	private void add() {
+		System.out.println("add");
+
+	}
+	public static void main(String[] args) {
+GitClass g = new GitClass();
+g.add();
+		
+	}
+
+}
+
